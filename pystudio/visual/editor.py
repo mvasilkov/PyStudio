0e7445ae7f24167f7c2ccd225d932f918b15ab56
Mark Vasilkov <mvasilkov@gmail.com>
2023-01-13 19:19:56 +0200

    Load model

--- pystudio/visual/editor.py
+++ pystudio/visual/editor.py
@@ -1,4 +1,13 @@
-from PySide6.QtWidgets import QFrame, QHeaderView, QScrollArea, QSplitter, QTreeWidget, QVBoxLayout, QWidget
+from PySide6.QtWidgets import (
+    QFrame,
+    QHeaderView,
+    QScrollArea,
+    QSplitter,
+    QTreeWidget,
+    QTreeWidgetItem,
+    QVBoxLayout,
+    QWidget,
+)
 
 from .builder import Builder
 from .poc.hello_dataclasses import hello
@@ -12,7 +21,7 @@ class VisualEditor(QWidget):
         layout.setContentsMargins(0, 0, 0, 0)
 
         splitter = QSplitter()
-        splitter.addWidget(StructurePane())
+        splitter.addWidget(StructurePane(hello))
 
         scroll = QScrollArea()
         scroll.setFrameShape(QFrame.Shape.NoFrame)
@@ -31,18 +40,41 @@ class VisualEditor(QWidget):
 
 
 class StructurePane(QWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, model, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.model = model
 
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
 
         tree = QTreeWidget()
         tree.setMinimumWidth(100)
-        tree.setHeaderLabels(['Component'])
+        tree.setHeaderLabels(['Component', 'Type'])
         tree.header().setSectionsMovable(False)
         tree.header().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+        tree.addTopLevelItem(self.loadModel(model))
         layout.addWidget(tree)
+
+    def loadModel(self, model, parent=None):
+        if model.type == 'QBoxLayoutStretch':
+            props = ['(stretch)', '']
+        else:
+            props = [model.name, model.type]
+
+        entry = QTreeWidgetItem(props)
+        if parent is not None:
+            parent.addChild(entry)
+
+        match model.type:
+            case 'QWidget':
+                self.loadModel(model.layout, entry)
+
+            case 'QHBoxLayout' | 'QVBoxLayout':
+                for child in model.children:
+                    self.loadModel(child, entry)
+
+        return entry
 
 
 class PropertiesPane(QWidget):
